b0efcf0d1ca297643fb7fe60e6e2d5a92faf46c4
yaf3ei1 <94067498+yaf3ei1@users.noreply.github.com>
2021-11-12 02:01:37 +0300

    Update anfas.js

--- anfas.js
+++ anfas.js
@@ -1,134 +1,4 @@
-if(location.protocol!='https:'){location.protocol='https:';}
 
-$(`<img src="https://www.ya-f3.com/sico/1636459719380.gif" class="fr" style="width:100%;margin-top: 0px;">
-
-<a id="des3" href="#" style="border-radius: 5px 0px 5px 0px;width:23%;margin:2px;" class="fl mini  fa  fa-
-
- btn btn-primary"> التطبيق</a>
-
-<a id="des3"#" style="border-radius: 5px 0px 5px 0px;width:23%;margin:2px;" class="fl mini  fa   fa-
-
- btn btn-default">الاشتراك</a>
-
-<a id="des3"#" style="border-radius: 5px 0px 5px 0px;width:27%;margin:2px;" class="fl mini  fa fa-
-
-btn btn-primary"> قوانين موقع</a>
-
-<a id="des3"#" style="border-radius: 5px 0px 5px 0px;width:22%;margin:2px;" class="fl mini  fa fa-
-
-btn btn-default"> اتصل بنا</a>
-
- `).insertBefore('.nav-tabs');
-
- $("div#tlogins button.btn.btn-primary").click(function(){var myVar = setInterval(function(){ var usmsgw = $(".pmsgc").length;if(usmsgw > 0){
-
-   $(`
-
-    <div class="uzr fl corner borderg mm" style="border-radius:5px;margin-bottom:-2px;width:99.5%;padding:0px; background-color:white;">
-
-      <img style="width: 36px; height: 38px; margin-left: 1px; margin-top: 1px; background-image: url(https://www.ya-f3.com/sico/1636459867541.jpg);" class="fl fitimg hand u-pic    ">
-
-      <div class="uzr fl" style="padding:0px;width:80%">
-
-        <div style="padding:0px;width:100%;" class="fl">
-
-          <img class="fl u-ico" alt="" src="    >
-
-          <span style="padding: 1px 8px; margin-top: 2px; display: block; max-width: 82%; border-radius: 3px; color: rgb(199, 103, 48);" class="corner nosel u-topic dots fl hand"> - : إدارة ، الشات  </span>
-
-        </div>
-
-        <br>
-
-        <div style="padding: 0px; width: 100%; color: rgb(60, 0, 255);" class=" u-msg   break  fl">
-
-        <div style="padding: 0px;width: 100%;color: rgb(60, 0, 255);text-align: left;display: block;margin-top: -17px;" class=" u-msg   break  fl"> مرحباً بك 👋 <h1 style="display: inline-block;color: red;">`+ getuser(myid).topic+ `</h1> نقدّر تواجدك معنا، ونتمنى لك يوماً سعيداً إن شاء الله في شات انفاس للجوال</div>
-
-      </div>
-
-    </div></div>
-
-    `).appendTo('div#d2')
-
-clearInterval(myVar);
-
-}else{console.log(usmsgw)} }, 2000);
-
-})
-
-$('.nav-tabs').addClass('fl').css('width','100%');
-
-$('a[href="https://jawalhost.com/"]').removeAttr('href');
-
-$(`<link href="https://fonts.googleapis.com/css?family=Aref+Ruqaa|El+Messiri" rel="stylesheet">
-
-<style>
-
-textarea.fl.filw.corner.tbox {border-radius: 10px;padding-left: 5px;border: 2px solid #adadad;margin-top: 2px;}
-
-.label.tc.border.btn.fl, .label.tc.border.btn.fl {border: 2px solid #316C7D;font-family: 'El Messiri',FontAwesome;border-radius: 3px;}
-
-</style>`).insertBefore('body');
-
-$('.hand.nosel.fl.uzr.border').css({'margin': '0px',borderRight: '3px solid #316C7D',borderLeft: '3px solid #316C7D',borderTop: '1px solid #e2e1e1',borderBottom: '1px solid #e2e1e1',borderRadius: '3px'});
-
-$(`<center><div><div width="75.5%" style="background-color: #b7c3c5; color: #2f4749;border-radius: 5px 5px 5px5px;border-bottom: 2px solid #ffffff;border-top: 2px solid#ffffff;padding-bottom: 2px!important;"  
-
-><font style="background-color: #fff;border-radius: 0px 25px 0px 25px;font-family: 'Cairo', sans-serif, FontAwesome;padding: 2px 10px 2px 10px; margin:0px 25px 0px 25px;" >اهلآ وسهلاً بكم فِي شآت انفاس للجوال  </font></div></div><center>`).insertBefore('#d2');
-
-$(`<img style="height:50px;width: 100%;margin-right: 5px;background-color: #2f4749;" src="https://www.ya-f3.com/sico/1636459694839.gif" border="0">`).insertBefore('div#d2bc');
-
-$('.break.light').css({"background-image":"url(https://www.ya-f3.com/sico/1636459867541.jpg)"});
-
-(document.getElementById("tbox").placeholder = "مرحبا بك في شات انفاس : اكتب رسالتك هنا ");
-
-$("div#tlogins button.btn.btn-primary").click(function() {
-
-    var c = setInterval(function() {
-
-        var a = $(".pmsgc").length;
-
-        0 < a ? ($(`<img style="height: 99.3%;
-
-width:99%;
-
-margin-top: 2px;
-
-pointer-events:none;
-
- margin-bottom: 5px;
-
- "src="https://www.ya-f3.com/sico/1636459867541.jpg"></div><center>`).appendTo("div#chats"), clearInterval(c)) : console.log(a)
-
-    }, 1)
-
-});
-
-$(`<button onfa-microphone="alert('قريبا')" style="width:31px;margin:2px;" style="margin-top:2px;margin-left:2px;" class="fa fa-video-camera fl btn btn-danger"></button>`).insertBefore('button.fr.fa.fa-share-alt.sndfile.fl.btn.btn-primary');
-
-$('.nav-tabs').addClass('fl').css('width','100%'),
-
-$(`<button style="width:24px;margin:2px;" style="margin-top:2px;margin-left:2px;" class="fa fa-microphone fl btn btn-info"></button>`).insertBefore('button.fr.fa.fa-share-alt.sndfile.fl.btn.btn-primary');
-
-$
-
-$(` <center><div width="99%" id="asim7"><font style="background-color:#FFFFFF;color:#2f4749;border-radius: 15px 15px 15px 15px;font-family: 'jazeera',FontAwesome;padding: 2px 10px 2px 10px;margin: 7px 10px 7px 10px;" > 
-
- ( متنفس الآقلأم والقلوب)  </font></div><center>`).insertBefore('div#d2bc');
-
-$(`<div style=" color: #FC1717;text-align: center;margin-bottom: 10px; font-family:'jazeera-light', Fontawesome"> إعلانات شات انفاس  للضروره فقط </div>`).insertBefore('span.fl.fa.fa-send');
-
-for(var k in navigator){
-
-if(typeof navigator[k]!= "function" && k!="n"){
-
-try{navigator.n[k]=JSON.parse(JSON.stringify(navigator[k]))}catch(er){}
-
-}
-
-}
-
-navigator.n.userAgentData=null;
 $("<div class='fr borderg' style='padding:2px;background-color:white;margin-right:4px;'> <a href='https://ya-f3.com/'>شات انفاس للجوال</a></div>").insertAfter($(".loginstat"))
 
 $("<div class='fr borderg' style='padding:2px;background-color:white;margin-right:4px;'> <a href='https://ya-f3.com/'>دردشة انفاس للجوال</a></div>").insertAfter($(".loginstat"))
@@ -137,10 +7,10 @@ $("<div class='fr borderg' style='padding:2px;background-color:white;margin-righ
 
 $("<div class='fr borderg' style='padding:2px;background-color:white;margin-right:4px;'> <a href='https://ya-f3.com/'>شات صوتي كتابي </a></div>").insertAfter($(".loginstat"))
 $(`<a href="https://instabio.cc/3102511mK1SUh" target="_blank" style=" width: 100%; "  id="asim33" class="btn btn-primary btn-sm" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample"> للتواصل عبر مواقعنا </div></a>`).insertBefore('a.fl.fa.fa-ban.btn.ui-corner-all.ui-shadow.ui-btn.ui-btn-inline.umute.borderg');
-$(` <center><div width="99%" id="asim7"><font style="background-color:#FFFFFF;color:#2f4749;border-radius: 15px 15px 15px 15px;font-family: 'jazeera',FontAwesome;padding: 2px 10px 2px 10px;margin: 7px 10px 7px 10px;" > 
-
-$(".tboxbc").attr("placeholder", "اطلق العنان لقلمك وابدع");
 $(`<div><marquee direction="right" width="100%" id="des0" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="4"> أهلاً وسهلا بكم في شات <font color="#357524" class="des2"> { أنفـآإس للجؤال </font>افضل حضور الاسبوع<font class="des2" color="#800000"> ( قريباً ) </font>مبدع الشات <font class="des2" color="#223366"> ( قريباً ) </font>افضل روم لهذا الاسبوع<font class="des2" color="#ff00ff"> ( قريباُ )   </font>افضل سوبر هذا الآسبوع  <font class="des2" color="#5CAD9A"> ( قريباً ) </font>مبدع الحائط لهذا الاسبوع<font class="des2" color="#800000"> قريباً </font>مع تمنياتنا للجميع باطيب الاوقات  </marquee></div>`).insertBefore('.lonline.light.break');
+$(` <center><div width="99%" id="asim7"><font style="background-color:#FFFFFF;color:#2f4749;border-radius: 15px 15px 15px 15px;font-family: 'jazeera',FontAwesome;padding: 2px 10px 2px 10px;margin: 7px 10px 7px 10px;" > 
+ ( متنفس الآقلأم والقلوب)  </font></div><center>`).insertBefore('div#d2bc');
+$(".tboxbc").attr("placeholder", "اطلق العنان لقلمك وابدع");
 $("#l1>button,#l2>button").click(function(){alert('   اهلآإ بكم فيء عائلتنا عائلة شات انفاس للجوال     ')})
 $(`<div style=" color: #FC1717;text-align: center;margin-bottom: 10px; font-family:'jazeera-light', Fontawesome"> إعلانات شات انفاس  للضروره فقط </div>`).insertBefore('span.fl.fa.fa-send');
 $(`<a href="https://coolnames.online/English-decoration" target="_blank" style=" width: 30%; "  id="asim33" class="btn btn-primary btn-sm" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">زخرفة</div></a>`).insertBefore('a.fl.fa.fa-ban.btn.ui-corner-all.ui-shadow.ui-btn.ui-btn-inline.umute.borderg');
