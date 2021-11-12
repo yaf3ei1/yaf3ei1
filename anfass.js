7a8b63f02632345c2acef0ffb8c32e10bbece649
yaf3ei1 <94067498+yaf3ei1@users.noreply.github.com>
2021-11-12 18:41:24 +0300

    Update anfass.js

--- anfass.js
+++ anfass.js
@@ -1,11 +1,6 @@
-$('.fl.ustat').css('width','3px'),$('.fa.fa-user-plus').html(`تسجِيل،العَضويةة`),document.getElementById('u1').placeholder='اكتبَ اسمك من 8 حروف',document.getElementById('pass1').placeholder='الرجاء تقويه كلمة المرور',document.getElementById('u2').placeholder='
+$('.fl.ustat').css('width','3px'),$('.fa.fa-user-plus').html(`تسجِيل،العَضويةة`),document.getElementById('u1').placeholder='اكتبَ اسمك من 8 حروف· ',document.getElementById('pass1').placeholder='الرجاء تقويه كلمة المرور',document.getElementById('u2').placeholder='فخأمة طلتكَ تنؤرنآ',document.getElementById('pass2').placeholder=' بآسوردك لاإ تنساهَ ي جميل ',document.getElementById('u3').placeholder='اكتبَ اسم العضوية ولاإزم تحفظها ';$("[data-target=#wall]").attr('title',' حآإئط / شأت أنفآإس·')
 
-فخَـأإمة طلتكَ تنؤرنآ',document.getElementById('pass2').placeholder=' بآسوردك لاإ تنساهَ يَ جمـيل ',document.getElementById('u3').placeholder='اكتبَ اسم العضوية ولاإزم تحَفظها ';$("[data-target=#wall]").attr('title',' حآإئط / شأت أنفآإس')
-
-$("[data-target=#settings]").attr('title','     الأعدإدإت و الضبط')
-
+$("[data-target=#settings]").attr('title','     الأعدادات و الضبط')
 $("[data-target=#rooms]").attr('title','   الغرف و الرومـات ')
-
 $("[data-target=#chats]").attr('title','      المحادثات الخاصه  ')
-
 $("[data-target=#users]").attr('title','  ضيـوفنـا المتـواجدين  ')
