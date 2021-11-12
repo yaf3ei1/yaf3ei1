4b841cd435a0346c1809796c084f6f5b72fd6f6b
yaf3ei1 <94067498+yaf3ei1@users.noreply.github.com>
2021-11-12 18:22:32 +0300

    Update anfas.js

--- anfas.js
+++ anfas.js
@@ -114,6 +114,17 @@ $("div#tlogins button.btn.btn-primary").click(function(){var b=setInterval(funct
 $(`<img tabindex="0" class="smoke fl"data-original-title="" title=""id="smoke3">
 `).insertAfter('img.fl.nosel.emobox');
 
+$('.fl.ustat').css('width','3px'),$('.fa.fa-user-plus').html(`تسجِيل،العَضويةة`),document.getElementById('u1').placeholder='اكتبَ اسمك من 8 حروف',document.getElementById('pass1').placeholder='الرجاء تقويه كلمة المرور',document.getElementById('u2').placeholder='
+
+فخَـأإمة طلتكَ تنؤرنآ',document.getElementById('pass2').placeholder=' بآسوردك لاإ تنساهَ يَ جمـيل ',document.getElementById('u3').placeholder='اكتبَ اسم العضوية ولاإزم تحَفظها ';$("[data-target=#wall]").attr('title',' حآإئط / شأت أنفآإس')
+
+$("[data-target=#settings]").attr('title','     الأعدإدإت و الضبط')
+
+$("[data-target=#rooms]").attr('title','   الغرف و الرومـات ')
+
+$("[data-target=#chats]").attr('title','      المحادثات الخاصه  ')
+
+$("[data-target=#users]").attr('title','  ضيـوفنـا المتـواجدين  ')
 $( "div#l1 button.btn.btn-primary" ).bind( "click", function() {
 alert( "اهلا وسهلا بكم في شات انفاس للجوال عند تسجيلك  لـ عضوية تحصل على 1000 لايك +  زخرفة + ريشه :: طريقة التسجيل اضغط على خيار (تسجيل) اكتب (اكتب اسمك المستعار + كلمة المرور ) وبعدها اضغط دخول" );
 });
