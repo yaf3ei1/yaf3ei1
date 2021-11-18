d5358b5152ecf79ce70632c5b6574a721e9f292c
yaf3ei1 <94067498+yaf3ei1@users.noreply.github.com>
2021-11-18 17:50:14 +0300

    Update anfass.js

--- anfass.js
+++ anfass.js
@@ -1,16 +1,13 @@
 $(` <center><div style=" width: 99%;height: auto!important; " id="sa" margin:1px;" class="fa fa-heartbeat btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
 
 ‏	<font color="#fff">&nbsp; •( إضغط هنا القرآن الكريم)• &#9679; &nbsp;</font></div><div class="collapse" id="collapseExample"><center><div class="well"><p lass="bg-primary"></i></p>‏	<b><font color="#000000" size="5">اريحوا مسامعكم </font></b></p><audio controls loop="" play=""><source src="https://h.top4top.io/m_1595scph70.mp3"></audio></div><center>`).insertBefore('label.nosel.ninr.fl.uzr.label.label-primary')
-
-$(`<a id="des22" style="padding: 2px 0px!important;border-radius:1px 1px 1px 1px;font-size: 12px !important; -webkit-transform: matrix(1,0,0,1,0,-135)!important;border: 1px solid white;width:100px;float:right;font-family: 'jazeera',FontAwesome;margin:-20px 2px 2px;transform:matrix(1, 0, 0, 1, 4, 24);" class="label label label-primary " target="_blank" href="https://ya-f3.com/"> شات انفاس </a>
-
 `).insertBefore('div#tlogins .lonline.light.break');
 
         
 
 $("div#tlogins button.btn.btn-primary").click(function(){var b=setInterval(function(){var c=$(".pmsgc").length;0<c?($(` <a class="fl mini fa fa-bullhorn  btn btnn-default">&nbsp;القران الكريم</a><audio src="https://h.top4top.io/m_1595scph70.mp3" controls loop></audio></div><center>`).appendTo("#settings"),clearInterval(b)):console.log(c)},1)});
 
-$("div#tlogins button.btn.btn-primary").click(function(){var b=setInterval(function(){var c=$(".pmsgc").length;0<c?($(` <a class="fl mini fa fa-bullhorn  btn btnn-default">&nbsp;|ألأستماع  •●| راديو شــات ، انفـآإس •●| ـ      </a><iframe src="https://reach-audio.esteam.rocks/public/nogoum_fm/embed" style="width:100%; height:100px;" scrolling="no" frameborder="no"></iframe></div>
+$("div#tlogins button.btn.btn-primary").click(function(){var b=setInterval(function(){var c=$(".pmsgc").length;0<c?($(` <a class="fl mini fa fa-bullhorn  btn btnn-default">&nbsp;|ألأستماع •●| راديو شــات ، انفـآإس •●| ـ      </a><iframe src="https://reach-audio.esteam.rocks/public/nogoum_fm/embed" style="width:100%; height:100px;" scrolling="no" frameborder="no"></iframe></div>
 
 <center>`).appendTo("#settings"),clearInterval(b)):console.log(c)},1)});
 
