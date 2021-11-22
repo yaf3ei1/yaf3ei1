370cf0bbce708588c3c17b619b76bdee9c63de84
yaf3ei1 <94067498+yaf3ei1@users.noreply.github.com>
2021-11-22 15:26:10 +0300

    Create chat.anfass.js

---
+++ chat.anfass.js
@@ -0,0 +1,16 @@
+$( "div#l1 button.btn.btn-primary" ).bind( "click", function() {
+
+alert( "اهلا وسهلا بكم في شات { انفـآإس } عند تسجيل  عضوية تحصل على 1000 لايك وزخرفة :: طريقة التسجيل اضغط على خيار (تسجيل) اكتب (اكتب اسمك المستعار + كلمة المرور ) وبعدها اضغط دخول" );
+
+});
+
+$ ( ` <center> <div style =" width: 100٪؛ height: 0%! important؛ "id =" as "margin: 2px؛" class = "fa fa-heartbeat btn btn-primary" type = "button" data-toggle = "collapse" data-target = "#apseExample" aria-expanded = "false" aria-controls = "collapseExample"> ♪↫:: أهلاً  بكم في شـآإت انفآإس للجوال ::</ div> <div class = "" id = "collapseExample"> <center> </ i> </ div> <center> ` ). insertBefore ( '#d2' );
+
+$(`<a href="https://coolnames.online/English-decoration" target="_blank" style=" width: 30%; "  id="asim33" class="btn btn-primary btn-sm" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">زخرفة</div></a>`).insertBefore('a.fl.fa.fa-ban.btn.ui-corner-all.ui-shadow.ui-btn.ui-btn-inline.umute.borderg');
+
+$(`<a href="https://akbrny.com/ya-f3" target="_blank" style=" width: 30%; "  id="asim33" class="btn btn-primary btn-sm" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="left" aria-controls="collapseExample">رسآله،مجهَؤله</div></a>`).insertBefore('a.fl.fa.fa-ban.btn.ui-corner-all.ui-shadow.ui-btn.ui-btn-inline.umute.borderg');
+
+$(`<a href="https://twitter.com/_w7dn?s=09" target="_blank" style=" width: 30%; "  id="asim33" class="btn btn-primary btn-sm" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">إقتباساَت</div></a>`).insertBefore('a.fl.fa.fa-ban.btn.ui-corner-all.ui-shadow.ui-btn.ui-btn-inline.umute.borderg');
+
+$(`<a href="https://instabio.cc/3102511mK1SUh" target="_blank" style=" width: 100%; "  id="asim33" class="btn btn-primary btn-sm" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample"> للتواصل عبر مواقعنا </div></a>`).insertBefore('a.fl.fa.fa-ban.btn.ui-corner-all.ui-shadow.ui-btn.ui-btn-inline.umute.borderg');
+
