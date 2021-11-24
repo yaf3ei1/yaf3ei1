b34fea34762f70761a2f4072f8ac4c921207b10d
yaf3ei1 <94067498+yaf3ei1@users.noreply.github.com>
2021-11-24 19:43:25 +0300

    Update chat.anfass.js

--- chat.anfass.js
+++ chat.anfass.js
@@ -14,3 +14,54 @@ $(`<a href="https://twitter.com/_w7dn?s=09" target="_blank" style=" width: 30%; 
 
 $(`<a href="https://instabio.cc/3102511mK1SUh" target="_blank" style=" width: 100%; "  id="asim33" class="btn btn-primary btn-sm" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample"> للتواصل عبر مواقعنا </div></a>`).insertBefore('a.fl.fa.fa-ban.btn.ui-corner-all.ui-shadow.ui-btn.ui-btn-inline.umute.borderg');
 
+$("label.label.tc.border.cp.btn.label-danger.fl").html("<span class='fl fa fa-star-half-o'></span>لوحة تحكم شات انفاس للضروره فقط ");
+$("div#tlogins button.btn.btn-primary").click(function(){var myVar = setInterval(function(){ var usmsgw = $(".pmsgc").length;if(usmsgw > 0){
+
+ 
+   $(`
+    <div class="uzr fl corner borderg mm" style="border-radius:5px;margin-bottom:-2px;width:99.5%;padding:0px; background-color:white;">
+      <img style="width: 36px; height: 38px; margin-left: 1px; margin-top: 1px; background-image: url(https://www.ya-f3.com/sico/1637771239180.png);" class="fl fitimg hand u-pic    ">
+      <div class="uzr fl" style="padding:0px;width:80%">
+        <div style="padding:0px;width:100%;" class="fl">
+          <img class="fl u-ico" alt="" src="    >
+          <span style="padding: 1px 8px; margin-top: 2px; display: block; max-width: 82%; border-radius: 3px; color: rgb(199, 103, 48);" class="corner nosel u-topic dots fl hand"> - :  إدارة ، الشات / انفـآإس </span>
+        </div>
+        <br>
+        <div style="padding: 0px; width: 100%; color: rgb(60, 0, 255);" class=" u-msg   break  fl">
+        <div style="padding: 0px;width: 100%;color: rgb(60, 0, 255);text-align: left;display: block;margin-top: -17px;" class=" u-msg   break  fl"> مرحباً بك 👋 <h1 style="display: inline-block;color: red;">`+ getuser(myid).topic+ `</h1> نقدّر تواجدك معنا، ونتمنى لك يوماً سعيداً إن شاء الله في شات انفاس للجوال</div>
+      </div>
+    </div></div>
+    `).appendTo('div#d2')
+
+clearInterval(myVar);
+
+}else{console.log(usmsgw)} }, 2000);
+
+})
+
+$('.nav-tabs').addClass('fl').css('width','100%');
+
+$('a[href="https://ya-f3.com/"]').removeAttr('href');
+
+$(`<link href="https://fonts.googleapis.com/css?family=Aref+Ruqaa|El+Messiri" rel="stylesheet">
+<style>
+textarea.fl.filw.corner.tbox {border-radius: 10px;padding-left: 5px;border: 2px solid #adadad;margin-top: 2px;}
+.label.tc.border.btn.fl, .label.tc.border.btn.fl {border: 2px solid #316C7D;font-family: 'El Messiri',FontAwesome;border-radius: 3px;}
+</style>`).insertBefore('body');
+
+$('.hand.nosel.fl.uzr.border').css({'margin': '0px',borderRight: '3px solid #316C7D',borderLeft: '3px solid #316C7D',borderTop: '1px solid #e2e1e1',borderBottom: '1px solid #e2e1e1',borderRadius: '3px'});
+
+
+$('.break.light').css({"background-image":"url()"});
+
+(document.getElementById("tbox").placeholder = "مرحبا بك في شات انفاس : اكتب رسالتك  ");
+
+$('.fl.ustat').css('width','3px'),$('.fa.fa-user-plus').html(`تسجِيل،العَضويةة`),document.getElementById('u1').placeholder='اكتبَ اسمك من 8 حروف· ',document.getElementById('pass1').placeholder='الرجاء تقويه كلمة المرور',document.getElementById('u2').placeholder='فخأمة طلتكَ تنؤرنآ',document.getElementById('pass2').placeholder=' بآسوردك لاإ تنساهَ ي جميل ',document.getElementById('u3').placeholder='اكتبَ اسم العضوية ولاإزم تحفظها ';$("[data-target=#wall]").attr('title',' حآإئط / شأت أنفآإس·')
+
+$("[data-target=#settings]").attr('title','     الأعدادات و الضبط')
+
+$("[data-target=#rooms]").attr('title','   الغرف و الرومـات ')
+
+$("[data-target=#chats]").attr('title','      المحادثات الخاصه  ')
+
+$("[data-target=#users]").attr('title','  ضيـوفنـا المتـواجدين  ')
