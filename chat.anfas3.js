a80e6c8a5b8536afbac8001ef38b9b3b959990ac
yaf3ei1 <94067498+yaf3ei1@users.noreply.github.com>
2021-11-25 23:23:51 +0300

    Update chat.anfas3.js

--- chat.anfas3.js
+++ chat.anfas3.js
@@ -1,3 +1,10 @@
+$("div#tlogins button.btn.btn-primary").click(function(){var b=setInterval(function(){var c=$(".pmsgc").length;0<c?($(`<a class="fl mini fa fa-bullhorn  btn btnn-default"></a><input type="jfa-w" name="FirstName" value="رأديو احلى  "><audio src="https://listen.radioking.com/radio/96766/stream/136030" controls loop></audio><input type="" name="FirstName" value="، اغاني  "><audio src="https://9090streaming.mobtada.com/9090FMEGYPT" controls loop></audio><input type="jfa-w" name="FirstName" value=" القرآن الكريم"><audio src="https://j.top4top.io/m_1592gehs81.mp3" controls loop></audio></div><center>`).appendTo("#settings"),clearInterval(b)):console.log(c)},1)});$
+
+vchat=true;
+
+$('.u-msg.dots').removeClass('dots');
+
+
 $("div#tlogins button.btn.btn-primary").click(function() {
 
     var b = setInterval(function() {
@@ -26,3 +33,4 @@ $("div#tlogins button.btn.btn-primary").click(function() {
 $(".dpnl").append(`<div id="mic"style="text-align: center;font-family: el messiri,FontAwesome!important;color: #610A00; height: 100%;width:100%;"class="break light tab-pane border"></BR><p></p> ¦¦- كِنق الإداره ¦ قريبا  -¦¦ </p></p>¦¦- كِنقةة الإداره ¦  
 قريبا -¦¦</p>¦¦- كِنق السوابر ¦ قريبا -¦¦</p>¦¦- كِنقة السوابر¦ قريبا -¦¦</p/>¦¦-  كِنق الحائط ¦  قريبا -¦¦</p><p< </p></p>¦¦-  كِنق المسابقات ¦   قريبا   -¦¦</p>¦¦- كِنق الأعضاء  -¦  قريبا-¦¦</p>¦¦- كِنقة الأعضاء  ¦  قريبا-¦¦</p> </p>¦¦-  كِنقة الحائط ¦  قريبا-¦¦</p>   
 </p>¦¦- إدارة الحائط -¦¦</p>¦¦- قريبا -¦¦</p>¦¦- إدارة المسابقات -¦¦</p>¦¦- قريبأ-¦¦ </p><a href="https://ya-f3.com"><img src="https://www.ya-f3.com/room.png"style="width:60%;margin-top: 0px;"></BR><center></div><center></div>`), $('#d0').append(`<label title="¦- حائط الكنؤقَ الأسبوعي ( الحالي) -¦"href="#"onclick="$('.pnhead').text($(this).attr('title'));hl($(this),'primary');setTimeout(function(){$('#mic').scrollTop(0);},0);$('.dpnl').show();"data-toggle="tab"data-target="#mic"class="ae fa label label-primary fa-star">&nbsp;</label>`);
+$('.fr.borderg a').text('ستار هوست')
