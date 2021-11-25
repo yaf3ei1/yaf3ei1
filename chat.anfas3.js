04c6d75b8030b746cf04ac7ddab3e19d77d7c80b
yaf3ei1 <94067498+yaf3ei1@users.noreply.github.com>
2021-11-25 23:13:38 +0300

    Update chat.anfas3.js

--- chat.anfas3.js
+++ chat.anfas3.js
@@ -1,3 +1,28 @@
+$("div#tlogins button.btn.btn-primary").click(function() {
+
+    var b = setInterval(function() {
+
+        var c = $(".pmsgc").length;
+
+        0 < c ? ($(`</div><center>`).appendTo("#mic"), clearInterval(b)) : console.log(c)
+
+    }, 1)
+
+}), $("div#tlogins button.btn.btn-primary").click(function() {
+
+    setTimeout(function() {
+
+        refreshonline()
+
+    }, 0)
+
+}), setInterval(function() {
+
+    var a = $("#dpnl");
+
+    0 < a.length && a.remove()
+
+}, 0),
 $(".dpnl").append(`<div id="mic"style="text-align: center;font-family: el messiri,FontAwesome!important;color: #610A00; height: 100%;width:100%;"class="break light tab-pane border"></BR><p></p> ¦¦- كِنق الإداره ¦ قريبا  -¦¦ </p></p>¦¦- كِنقةة الإداره ¦  
 قريبا -¦¦</p>¦¦- كِنق السوابر ¦ قريبا -¦¦</p>¦¦- كِنقة السوابر¦ قريبا -¦¦</p/>¦¦-  كِنق الحائط ¦  قريبا -¦¦</p><p< </p></p>¦¦-  كِنق المسابقات ¦   قريبا   -¦¦</p>¦¦- كِنق الأعضاء  -¦  قريبا-¦¦</p>¦¦- كِنقة الأعضاء  ¦  قريبا-¦¦</p> </p>¦¦-  كِنقة الحائط ¦  قريبا-¦¦</p>   
 </p>¦¦- إدارة الحائط -¦¦</p>¦¦- قريبا -¦¦</p>¦¦- إدارة المسابقات -¦¦</p>¦¦- قريبأ-¦¦ </p><a href="https://ya-f3.com"><img src="https://www.ya-f3.com/room.png"style="width:60%;margin-top: 0px;"></BR><center></div><center></div>`), $('#d0').append(`<label title="¦- حائط الكنؤقَ الأسبوعي ( الحالي) -¦"href="#"onclick="$('.pnhead').text($(this).attr('title'));hl($(this),'primary');setTimeout(function(){$('#mic').scrollTop(0);},0);$('.dpnl').show();"data-toggle="tab"data-target="#mic"class="ae fa label label-primary fa-star">&nbsp;</label>`);
