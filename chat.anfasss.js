448528b7813f69851f6dffff52c5a05e121f8c4e
yaf3ei1 <94067498+yaf3ei1@users.noreply.github.com>
2021-11-30 23:31:03 +0300

    znznad.js

---
+++ chat.anfasss.js
@@ -0,0 +1,87 @@
+$( "div#l1 button.btn.btn-primary" ).bind( "click", function() {
+
+alert( "اهلا وسهلا بكم في شات { انفـآإس } عند تسجيل  عضوية تحصل على 1000 لايك وزخرفة :: طريقة التسجيل اضغط على خيار (تسجيل) اكتب (اكتب اسمك المستعار + كلمة المرور ) وبعدها اضغط دخول" );
+
+});
+
+$ ( ` <center> <div style =" width: 100٪؛ height: 0%! important؛ "id =" as "margin: 2px؛" class = "fa fa-heartbeat btn btn-primary" type = "button" data-toggle = "collapse" data-target = "#apseExample" aria-expanded = "false" aria-controls = "collapseExample"> ♪↫:: أهلاً  بكم في شـآإت انفآإس للجوال ::</ div> <div class = "" id = "collapseExample"> <center> </ i> </ div> <center> ` ). insertBefore ( '#d2' );
+
+$(`<a href="https://coolnames.online/English-decoration" target="_blank" style=" width: 30%; "  id="asim33" class="btn btn-primary btn-sm" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">زخرفة</div></a>`).insertBefore('a.fl.fa.fa-ban.btn.ui-corner-all.ui-shadow.ui-btn.ui-btn-inline.umute.borderg');
+
+$(`<a href="https://akbrny.com/ya-f3" target="_blank" style=" width: 30%; "  id="asim33" class="btn btn-primary btn-sm" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="left" aria-controls="collapseExample">رسآله،مجهَؤله</div></a>`).insertBefore('a.fl.fa.fa-ban.btn.ui-corner-all.ui-shadow.ui-btn.ui-btn-inline.umute.borderg');
+
+$(`<a href="https://twitter.com/_w7dn?s=09" target="_blank" style=" width: 30%; "  id="asim33" class="btn btn-primary btn-sm" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">إقتباساَت</div></a>`).insertBefore('a.fl.fa.fa-ban.btn.ui-corner-all.ui-shadow.ui-btn.ui-btn-inline.umute.borderg');
+
+$(`<a href="https://instabio.cc/3102511mK1SUh" target="_blank" style=" width: 100%; "  id="asim33" class="btn btn-primary btn-sm" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample"> للتواصل عبر مواقعنا </div></a>`).insertBefore('a.fl.fa.fa-ban.btn.ui-corner-all.ui-shadow.ui-btn.ui-btn-inline.umute.borderg');
+
+$("label.label.tc.border.cp.btn.label-danger.fl").html("<span class='fl fa fa-star-half-o'></span>لوحة تحكم شات انفاس للضروره فقط ");
+
+$("div#tlogins button.btn.btn-primary").click(function(){var myVar = setInterval(function(){ var usmsgw = $(".pmsgc").length;if(usmsgw > 0){
+
+ 
+
+   $(`
+
+    <div class="uzr fl corner borderg mm" style="border-radius:5px;margin-bottom:-2px;width:99.5%;padding:0px; background-color:white;">
+
+      <img style="width: 36px; height: 38px; margin-left: 1px; margin-top: 1px; background-image: url(https://www.ya-f3.com/sico/1636459867541.jpg);" class="fl fitimg hand u-pic    ">
+
+      <div class="uzr fl" style="padding:0px;width:80%">
+
+        <div style="padding:0px;width:100%;" class="fl">
+
+          <img class="fl u-ico" alt="" src="    >
+
+          <span style="padding: 1px 8px; margin-top: 2px; display: block; max-width: 82%; border-radius: 3px; color: rgb(199, 103, 48);" class="corner nosel u-topic dots fl hand"> - :  إدارة ، الشات / انفـآإس </span>
+
+        </div>
+
+        <br>
+
+        <div style="padding: 0px; width: 100%; color: rgb(60, 0, 255);" class=" u-msg   break  fl">
+
+        <div style="padding: 0px;width: 100%;color: rgb(60, 0, 255);text-align: left;display: block;margin-top: -17px;" class=" u-msg   break  fl"> مرحباً بك 👋 <h1 style="display: inline-block;color: red;">`+ getuser(myid).topic+ `</h1> نقدّر تواجدك معنا، ونتمنى لك يوماً سعيداً إن شاء الله في شات انفاس للجوال</div>
+
+      </div>
+
+    </div></div>
+
+    `).appendTo('div#d2')
+
+clearInterval(myVar);
+
+}else{console.log(usmsgw)} }, 2000);
+
+})
+
+$('.nav-tabs').addClass('fl').css('width','100%');
+
+$('a[href="https://ya-f3.com/"]').removeAttr('href');
+
+$(`<link href="https://fonts.googleapis.com/css?family=Aref+Ruqaa|El+Messiri" rel="stylesheet">
+
+<style>
+
+textarea.fl.filw.corner.tbox {border-radius: 10px;padding-left: 5px;border: 2px solid #adadad;margin-top: 2px;}
+
+.label.tc.border.btn.fl, .label.tc.border.btn.fl {border: 2px solid #316C7D;font-family: 'El Messiri',FontAwesome;border-radius: 3px;}
+
+</style>`).insertBefore('body');
+
+$('.hand.nosel.fl.uzr.border').css({'margin': '0px',borderRight: '3px solid #316C7D',borderLeft: '3px solid #316C7D',borderTop: '1px solid #e2e1e1',borderBottom: '1px solid #e2e1e1',borderRadius: '3px'});
+
+$('.break.light').css({"background-image":"url()"});
+
+(document.getElementById("tbox").placeholder = "مرحبا بك في شات انفاس : اكتب رسالتك  ");
+
+$('.fl.ustat').css('width','3px'),$('.fa.fa-user-plus').html(`تسجِيل،العَضوية`),document.getElementById('u1').placeholder='اكتبَ اسمك من 8 حروف· ',document.getElementById('pass1').placeholder='الرجاء تقويه كلمة المرور',document.getElementById('u2').placeholder='فخأمة طلتكَ تنؤرنآ',document.getElementById('pass2').placeholder=' بآسوردك لاإ تنساهَ ي جميل ',document.getElementById('u3').placeholder='اكتبَ اسم العضوية ولاإزم تحفظها ';$("[data-target=#wall]").attr('title',' حآإئط / شأت أنفآإس·')
+
+$("[data-target=#settings]").attr('title','     الأعدادات و الضبط')
+
+$("[data-target=#rooms]").attr('title','   الغرف و الرومـات ')
+
+$("[data-target=#chats]").attr('title','      المحادثات الخاصه  ')
+
+$("[data-target=#users]").attr('title','  ضيـوفنـا المتـواجدين  ')
+
+$(`${' <center><div style=" width: 99%;height: auto!important; " id="sa" margin:1px;" class="fa fa-heartbeat btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">\x0D\x0A‏	<font color="#fff">&nbsp; •( إضغط هنا القرآن الكريم)• &#9679; &nbsp;</font></div><div class="collapse" id="collapseExample"><center><div class="well"><p lass="bg-primary"></i></p>\x0D\x0A‏	<b><font color="#000000" size="5">اريحوا مسامعكم </font></b></p><audio controls loop="" play=""><source src="https://h.top4top.io/m_1595scph70.mp3"></audio></div><center>'}`)[
