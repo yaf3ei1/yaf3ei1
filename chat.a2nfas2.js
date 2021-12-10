9ffbc66c251b8e635152abc57b4a0577e4b1347e
yaf3ei1 <94067498+yaf3ei1@users.noreply.github.com>
2021-12-10 23:42:38 +0300

    laith.css

--- chat.a2nfas2.js
+++
@@ -1,9 +0,0 @@
-</style>`, 'a.fl.fa.fa-ban.btn.ui-corner-all.ui-shadow.ui-btn.ui-btn-inline.umute.borderg', '', `<!--<a class="fl fa fa-image btn ui-corner-all ui-shadow ui-btn ui-btn-inline borderg" style="color:purple;margin:2px;width: 106px;text-align: center;font-size:15px!important;"><span dir="rtl"> (0) </span><span style="font-size: 15px!important;>omar alhaded </span></a>-->
-\
-<a href="" target="_blank" style="width:107px; margin-right: 5px;
-\
-margin-left: 2px;text-align: center;float: right;border-radius: 5px;
-\
-border-color: #990033;
-\
-padding: 3px;font-family: 'Cairo', sans-serif, Fontawesome;font-size: 13px!important;" class="SLPBeats fab fa-angellist" >  ستايل وأرشفة : زنآإد،اليآإفعيء </a>`):
