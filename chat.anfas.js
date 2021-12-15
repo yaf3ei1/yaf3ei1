9459498b471dc80ffa26bce1b6b65bd75f328628
yaf3ei1 <94067498+yaf3ei1@users.noreply.github.com>
2021-12-15 11:19:22 +0300

    chat.yaf3ei.js

---
+++ chat.anfas.js
@@ -0,0 +1,136 @@
+
+    * {
+    font-family: serif;
+    font-weight: bold;
+    text-shadow: none!important;
+    font-size: 15.2px !important;line-height: 1.3!important;  
+}
+    .ae { 
+      border: 1px solid black!important; 
+      border-radius: 2px!important; 
+      float: left;
+      text-align: center;width: 20%; max-width: 80px;
+      padding: 4px 0px;
+      margin: 1px;margin-bottom: 2px;
+    }
+    .pmsgc {
+      background-color: rgba(0, 77, 255, 0.08)!important; 
+    }
+    .ppmsgc {
+      background-color: #f1f1ff!important;
+    }
+    .hmsg {
+      background-color: linen!important;
+    }
+    .label-primary,
+    .btn-primary,
+    .bg-primary,
+    .label-primary:hover,
+    .btn-primary:hover,
+    .btn-primary:focus {
+      background-color: #438ac7;
+      background-image: none;
+    } 
+    .bg {
+      background-color: #39536E;
+    } 
+    .bgg {
+      background-color: lightslategray;
+    } 
+    .pophead {
+      background-color: slategrey;
+    }
+    
+    .light {
+      background-color: #FEFEFE;
+    }
+    .label,.btn{
+      border-radius: 1px;
+    }
+    .label-primary,.btn-primary{  
+      background-color: #4B6A8C!important;
+    }
+    .hid {
+      display: none;
+    }
+    #mic .mic,#muteall{
+      background-color: #4B6A8C;
+    }
+    .typingIndicatorBubbleDot {
+  width: 4px;
+  height: 4px;
+  margin-right: 4px;
+  background-color: #57585a;
+  border-radius: 50%;
+  animation-name: bounce;
+  animation-duration: 1.3s;
+  animation-timing-function: linear;
+  animation-iteration-count: infinite;
+}
+.mic{
+  width:56px;height:50px;margin:0px;border-radius:4px;background-image: url(imgs/mic.png);background-size: cover;background-repeat: no-repeat;background-position: center;
+}
+.typingIndicatorBubbleDot:first-of-type {
+  margin: 0px 4px;
+}
+
+.typingIndicatorBubbleDot:nth-of-type(2) {
+  animation-delay: 0.15s;
+}
+
+.typingIndicatorBubbleDot:nth-of-type(3) {
+  animation-delay: 0.3s;
+}
+.flex-grow-1 {
+  flex-grow: 1 !important;
+}
+.c-flex {
+  display: flex;
+    flex-direction: column;
+}
+.d-flex{
+  display: flex; 
+}
+.flex-fill {
+  flex: 1 1 auto !important;
+}
+.bgf{
+  backdrop-filter: blur(8px);
+  -webkit-backdrop-filter: blur(8px);
+}
+@supports not ((-webkit-backdrop-filter: none) or (backdrop-filter: none)) {
+  .bgf {
+    background-color: rgba(0, 0, 0, .5);
+  }
+}
+.nav-pills li a{border-radius: 0px!important;}
+    @keyframes bounce {
+  0%,
+  60%,
+  100% {
+    transform: translateY(0);
+  }
+  30% {
+    transform: translateY(-4px);
+  }
+} 
+td {
+  border: 1px solid grey;
+  font-weight: bold;
+}  
+th {
+  background-color: cornflowerblue;
+  color: white;
+  padding: 5px;
+}
+.tab-pane{
+  width: 100%;
+}
+ 
+html {
+    overflow: hidden;
+} 
+.tc{
+  border-radius: 3px!important;
+}
+ 
