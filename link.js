f42b8774a3ed2f62b1b91745a924904bc34cf5c6
yaf3ei1 <94067498+yaf3ei1@users.noreply.github.com>
2021-12-15 11:27:12 +0300

    link.js

---
+++ link.js
@@ -0,0 +1,17 @@
+<script> 
+    window.port=1006
+    window.scrollTo(0,1);
+  </script>
+  <link rel="stylesheet" href="bootstrap.min.css">
+  <link rel="stylesheet" href="bootstrap-theme.min.css">
+  <link rel="stylesheet" href="css/font-awesome.min.css">
+  <link rel="stylesheet" href="style.css?14">
+  <script src="jquery-1.11.1.min.js"></script>
+  <script src="bootstrap.min.js"></script> 
+  <script type="text/javascript" src="jscolor/jscolor.js" async></script> 
+  <script type="text/javascript" src="janus/adapter.min.js" async></script> 
+  <script type="text/javascript" src="janus/janus.js" async></script>  
+  <script src="socket.io.2.0.3.js"></script>
+  <script type='text/javascript' src='jquery.tablesorter.min.js' async></script> 
+  <script src="x.js?28"></script>
+  <style>
