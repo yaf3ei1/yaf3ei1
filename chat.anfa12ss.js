fd1e17614e8e70642b0f8b29ea1765be4fb2b568
yaf3ei1 <94067498+yaf3ei1@users.noreply.github.com>
2021-12-15 22:53:27 +0300

    chat-znad.js

---
+++ chat.anfa12ss.js
@@ -0,0 +1,45 @@
+$('.nav-tabs').addClass('fl').css('width','121%'); $('.nav-tabs').addClass('fl').css('width','100%'); $('.nav-tabs').addClass('fl').css('width','100%');
+$(`<link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera-light,jazeera">
+<link rel="stylesheet" href="">
+<style>
+
+.light { background: repeating-linear-gradient(45deg,rgba(255,255,255,.8),rgb(255,255,255) 100px,#ffffffb0 0,#fff6f699 200px),repeating-linear-gradient(-45deg,rgba(255,255,255,.5),rgba(255,255,255,.93) 100px,#b3adadd1 0,#ffffffe6 200px)!important}
+
+.fl.u-pic{ height:42px!important; width:42px!important;}
+
+#as {font-family: 'jazeera-light',FontAwesome;margin-bottom: 2px; color: #9A5F98; border-radius: 5px 5px 5px 5px;
+    text-shadow: 2px 2px 2px rgb(138, 138, 138) , 0px 2px 10px #dad9d9 !important;
+border: 1px solid #9A5F98;
+height: 33px;
+text-align: center; 
+}
+.well {font-family: 'jazeera',FontAwesome;font-size: 16px!important;padding: 15px!important;text-align: center!important;width: 101%;color: #9A5F98;white-space: nowrap;border-radius: 14px 14px 14px 14px;}
+.border {shadow:1px 1px 1px 1px ridge rgb(137, 137, 137); box-shadow:1px 1px 1px 1px rgb(137, 137, 137) }
+.fitimg.fl.u-pic  {box-shadow: 0 1px 1px -1px rgb(137, 137, 137), 0 0 5px rgb(137, 137, 137);border-radius: 5px 5px 5px 5px;;}
+ul.nav.nav-tabs.fl li a {      color: #9A5F98;  box-shadow: inset 0 3px 3px rgb(137, 137, 137), 0 0 5px rgb(137, 137, 137);font-family: 'jazeera',FontAwesome;font-size: 12px!important;padding: 5px!important;text-align: center!important;width: 100%;}
+ul.nav.nav-tabs.fl li {border-bottom: 2px solid rgb(138, 137, 137);width: 33.3%;}
+textarea.fl.filw.corner.tbox {    box-shadow:inset 0 0px 0px rgb(137, 137, 137); 0 0 7px rgb(137, 137, 137);border-radius: 7px;padding-left: 5px;}
+label.label.tc.border.btn.fl {border-radius: 41px 21px 41px 21px;}
+.label.tc.border.btn.fl, .label.tc.border.btn.fl {border: 1px solid #9A5F98;font-family: 'jazeera',FontAwesome;border-radius: 4px 4px 4px 4px;}
+label.fl.nosel.label.pnhead {font-family: 'jazeera',FontAwesome;width: 81%;text-align: -webkit-center;background-color: #9A5F98;color: white;border-radius:13px 13px 13px 13px;}
+.fr.borderg a {color: #9A5F98;font-size: 12px!important;}
+.fr.borderg {color: #9A5F98;font-size: 13px!important;border: 1px solid #dad9d9;font-family: 'jazeera',FontAwesome;border-radius: 4px 4px 4px 4px;margin-top: 1px;}
+label.label-primary {border-radius: px 31px 31px 31px;}
+button.btn.fr.btn-success.fa.fa-refresh {
+padding: 4px 5px;
+background-color:"url()"});
+background-image::"url()"});
+border: 1px solid transparent;
+margin-top: -3px;
+border-radius: 50px;
+as:51px;
+}
+if(location.protocol!='https:'){location.protocol='https:';}
+
+.border.corner, .border.corner {box-shadow: inset 0 0px 0px rgb(137, 137, 137), 0 0 4px rgb(137, 137, 137);border: 2px solid #9A5F98;as:13px;}
+.label-primary, .btn-primary, .ae.fa.label.fa-commenting-o.label-warning, .ae.fa.chats.label.fa-comment.label-warning {border: 1px solid #9A5F98;border-radius: 31px 31px 31px 31px;as:31px;}
+.checkbox {color: rgb(137, 137, 137);}
+ </style>`).insertBefore('body');
+ 
+ $(`<style>\x0D\x0Adiv#tlogins img.fitimg.fl.u-pic{border-radius:50px!important;animation:move 2.9s ease 1s infinite normal}@keyframes move{0%{transform:scale(1.2) rotate(360deg);box-shadow:0 0 40px #9A5F98}50%{transform:scale(1.1) rotate(0deg);box-shadow:0 0 70px #9A5F98}100%{transform:scale(1) rotate(0deg);box-shadow:0 0 40px #9A5F98}}\x0D\x0A</style>
+</style>`).insertBefore('body');
