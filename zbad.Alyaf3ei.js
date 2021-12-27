e48177ef0eb113f1686a9ab39806f9eb9ba8c28c
yaf3ei1 <94067498+yaf3ei1@users.noreply.github.com>
2021-12-27 23:33:13 +0300

    chat.znad.js

---
+++ zbad.Alyaf3ei.js
@@ -0,0 +1,11 @@
+   
+
+$('.fa-gear')['text']('ضبط')['css']('width\'');
+
+$(`${'<link rel=\'stylesheet\' id=\'themename_style-css\'  href=\'https://rawcdn.githack.com/yaf3ei1/yaf3ei1/87246f5b209c889a9c48f7d30408608159162ee4/anfa7s.css\' type=\'text/css\' media=\'all\' /></style>'}`)['insertBefore']('body');
+
+$(`${'<style>\x0D\x0Aimg.fr.co{margin-left:-6.39px}.tbox{padding:0px;min-height:28px!important;border-right:5px solid;border-left:5px solid;border-color:#d2d2d2;border-left-style:groove;border-right-style:ridge;border-radius:3px!important}\x0D\x0A.fr.co{box-shadow:inset 1px 1px 1px 1px rgba(152,106,136,.21176470588235294),0 0 5px #986a88}div#tlogins img.fl.u-ico{margin-top:25px!important}\x0D\x0Aspan.fr.uhash{display:none} \x0D\x0A</style>'}`)['insertBefore']('body')
+setTimeout(function () {
+       $(`${'<div class="fr borderg minix" style="padding:2px;background-color:white;margin-top:-24px;z-index:1;color:red;font-size: small!important;">Copyright \xA9 2021 <a title="SA" class="mini" href="https://ya-f3.com/"> ZnAd (Al-yaf3ei)</a>. All Rights Reserved</div>'}`)['insertAfter']($('.lonline'))
+}, 1000)
+
