e3365809ed44ea13fb0d2617825f1b27432a2457
yaf3ei1 <94067498+yaf3ei1@users.noreply.github.com>
2021-12-27 17:40:25 +0300

    chat.yaf3ei.js

---
+++ yaf3ei.1.js
@@ -0,0 +1,26 @@
+$(`${'<style>\x0D\x0Abutton.btn.fr.btn-success.fa.fa-refresh{display:none}\x0D\x0A.checkbox{color:#0F47FF}div#l1,div#l2,div#l3{width:90%!important;margin-top:-2.8px!important;margin-left:0px!important;height:99px;border-radius:7px;top:0px;position:absolute;background-image:-webkit-linear-gradient(45deg,rgba(114,111,118,0) 25%,rgba(130,130,130,0) 25%,rgb(208,189,202) 50%,rgba(255,255,255,.2) 50%,rgba(255,255,255,.2) 75%,transparent 75%,transparent)!important}\x0D\x0A</style>'}`)['insertBefore']('body')
+
+$(`${'<style>\x0D\x0A.broadcasters {\xA0\xA0 background: #051626 !important;border-bottom: 2px solid #ca918f !important;background-image: url(sico/1640264095233.gif?z699)!important;}\x0D\x0A</style>'}`)['insertBefore']('body')
+
+$(`${'<style>\x0D\x0A.fl.nosel.label.pnhead{border:1px dashed#e75c8b!important;background:#a5a1a3!important;border-radius:20px;font-family:\'jazeera\',FontAwesome;font-size:14px!important;box-shadow:inset -1px -1px 1px rgb(220 104 143),0 0 8px rgb(255 255 255)}.broadcasters>div>.prod{background-image:url(sico/1639238585951.gif);border-radius:15px 5px!important}\xA0 .hand.nosel.fl.uzr.border{width:99%!important;margin-top:5px!important;border-radius:3px!important;border:1px solid #1391a740}body{background-image:url(sico/1639485952141.gif);background-size:contain;background-color:#f9f9f9!important}\xA0 img.fitimg.fl.borderg.spic.corner.hand { background-size: 100% 100%; width:80px!important; padding:20px!important; margin-left: 18%!important; border: 2px solid #e9598a7d!important; border-radius: 100px!important; background-image: url(1639485952141.gif); }\xA0 a#asim27{text-decoration:none;font-size:8px!important;color:#75aeb4!important;background:#fff0!important;height:12px;margin:0 0 0 19px;line-height:9px;position:absolute;top:153px;width:148px} div#tlogins .hand.nosel.fl.uzr.borderg{ background-image: url(sico/1639075469587.gif)!important } img.fitimg.fl.borderg.spic.corner.hand { background-size: 100% 100%; width:80px!important; padding:20px!important; margin-left: 18%!important; border: 2px solid #e9598a7d!important; border-radius: 100px!important; background-image: url(pic.png); } \x0D\x0A</style>'}`)['insertBefore']('body')
+
+$[_0x8489[1]](_0x8489[0]);
+
+var _0x82d7 = ['.nav-tabs', 'insertBefore', '<img src="/sico/1623138096611.gif" class="fr" style="width:100%;margin-top: -5px;" ></a> ', 'url(/sico/1631135298921.jpg', 'css', '.break.light'];
+
+$(`${''}${_0x82d7[2]}${''}`)[_0x82d7[1]](_0x82d7[0]);
+
+$(_0x82d7[5])[_0x82d7[4]]({
+
+    "background-image": _0x82d7[3]
+
+})
+
+$(`${'<style>\x0D\x0Aimg.fr.co{margin-left:-6.39px}.tbox{padding:0px;min-height:28px!important;border-right:5px solid;border-left:5px solid;border-color:#d2d2d2;border-left-style:groove;border-right-style:ridge;border-radius:3px!important}\x0D\x0A.fr.co{box-shadow:inset 1px 1px 1px 1px rgba(152,106,136,.21176470588235294),0 0 5px #986a88}div#tlogins img.fl.u-ico{margin-top:25px!important}\x0D\x0Aspan.fr.uhash{display:none} \x0D\x0A</style>'}`)['insertBefore']('body')
+
+setTimeout(function () {
+
+    $(`${'<div class="fr borderg minix" style="padding:2px;background-color:white;margin-top:-24px;z-index:1;color:red;font-size: small!important;">Copyright \xA9 2021 <a title="SA" class="mini" href="https://abo-arish.net"> SA-Mahdi</a>. All Rights Reserved</div>'}`)['insertAfter']($('.lonline'))
+
+}, 1000)
+
